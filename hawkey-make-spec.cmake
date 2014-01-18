808e2089d241440d5c4da304b08b7bcabe3b9d57
Radek Holy <rholy@redhat.com>
2014-01-18 21:22:34 +0100

    Add hawkey SPEC maker.

---
+++ hawkey-make-spec.cmake
@@ -0,0 +1,25 @@
+# Make the hawkey's SPEC file.
+# Usage: cmake -P hawkey-make-spec.cmake
+#
+# Copyright (C) 2014  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+
+CMAKE_MINIMUM_REQUIRED (VERSION 2.4)
+
+INCLUDE (${CMAKE_SOURCE_DIR}/VERSION.cmake)
+SET (VERSION "${HAWKEY_MAJOR}.${HAWKEY_MINOR}.${HAWKEY_PATCH}")
+
+CONFIGURE_FILE(${CMAKE_SOURCE_DIR}/package/hawkey.spec.in ${CMAKE_SOURCE_DIR}/package/hawkey.spec @ONLY)
