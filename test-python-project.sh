1c4cd41ab4a83436cebb432804b8d4270f5232a3
Radek Holy <rholy@redhat.com>
2014-01-20 15:08:13 +0100

    Add python tester.

---
+++ test-python-project.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+# Run python tests.
+# Usage: ./test-python-project.sh PYTHON_VERSION
+#
+# Copyright (C) 2014  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+
+export PYTHONPATH=".:$PYTHONPATH"
+
+# Run tests with default environment.
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+nosetests-$1 --quiet .; EXIT1=$?
+
+# Run tests with cs locale.
+export LANG=cs_CZ.utf8
+export LC_ALL=cs_CZ.utf8
+nosetests-$1 --quiet .; EXIT2=$?
+
+# Run tests without capturing the output.
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+nosetests-$1 --quiet --nocapture .; EXIT3=$?
+
+exit $(($EXIT1 + $EXIT2 + $EXIT3))
